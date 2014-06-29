d5e9e29ddd6dcb2e2ab61cd607d44d506a6ad639
The Developer <test@test.test>
2014-06-29 22:27:55 +0300

    added the project

---
+++ Demo/Example.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo
+{
+    using log4net;
+    using log4net.Config;
+
+    /// <summary>
+    /// Demo class just for the homework purposes.
+    /// </summary>
+    public class Example
+    {
+        /// <summary>
+        /// This field holds the instance of the logger.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Example));
+        
+        /// <summary>
+        /// Demonstrates logging using log4net.
+        /// </summary>
+        public static void Main()
+        {
+            BasicConfigurator.Configure();
+            Log.Debug("Debug msg");
+            Log.Error("Error msg");
+        }
+    }
+}
